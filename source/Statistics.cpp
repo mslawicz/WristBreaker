742418b30adfafbd3aca54f59c59b807efa01b77
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-10-16 19:25:47 +0200

    Statistics refactored

--- source/Statistics.cpp
+++ source/Statistics.cpp
@@ -9,6 +9,7 @@
 #include "mbed_stats.h"
 #include "Statistics.h"
 #include <iostream>
+#include <vector>
 
 #if !defined(MBED_THREAD_STATS_ENABLED)
 #error "Stats not enabled"
@@ -16,17 +17,18 @@
 
 void listThreads(const CommandVector&  /*cv*/)
 {
-    auto* stats = new mbed_stats_thread_t[MAX_THREAD_STATS];    // NOLINT(cppcoreguidelines-owning-memory)
-    int numberOfThreads = mbed_stats_thread_get_each(stats, MAX_THREAD_STATS);
+    auto* statsArray = new mbed_stats_thread_t[MAX_THREAD_STATS];    // NOLINT(cppcoreguidelines-owning-memory)
+    int numberOfThreads = mbed_stats_thread_get_each(statsArray, MAX_THREAD_STATS);
+    std::vector<mbed_stats_thread_t> threads(statsArray, statsArray + numberOfThreads);  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
     std::cout << "ID,Name,State,Priority,Stack size, Stack space\r\n";
-    for(int i = 0; i < numberOfThreads; i++)
+    for(auto thread : threads)
     {
-        std::cout << std::hex << "0x" << stats[i].id << ","  // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        << std::dec << stats[i].name << ","   // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        << stats[i].state << ","   // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        << stats[i].priority << ","   // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        << stats[i].stack_size << ","   // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-        << stats[i].stack_space << std::endl;   // NOLINT(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+        std::cout << std::hex << "0x" << thread.id << ","
+        << std::dec << thread.name << ","
+        << thread.state << ","
+        << thread.priority << ","
+        << thread.stack_size << ","
+        << thread.stack_space << std::endl;
     }
 }
