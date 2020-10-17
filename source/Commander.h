7127f5660cb6223da2a6c7953e1d0fe6dd3167b9
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-10-17 20:19:25 +0200

    USB.h is Lint clear

--- source/Commander.h
+++ source/Commander.h
@@ -23,7 +23,7 @@ private:
     const std::chrono::milliseconds HandlerPeriod{10};     // period of the handler calls
     uint32_t handlerCallCounter{0};             // counter of the handler calls 
     MultiHID PCLink;                            // USB link to PC
-    JoystickData joystickData;                  // structure of joystick data to be sent to PC
+    JoystickData joystickData{0};               // structure of joystick data to be sent to PC
 };
 
 #endif /* COMMANDER_H_ */
--- source/USB.h
+++ source/USB.h
@@ -29,14 +29,19 @@ class MultiHID : public USBHID
 {
 public:
     MultiHID(uint16_t vendorId, uint16_t productId, uint16_t productRelease, bool blocking = false);
-    virtual ~MultiHID();
+     ~MultiHID() override;
+    MultiHID(MultiHID const&) = delete;
+    void operator=(MultiHID const&) = delete;
+    MultiHID(MultiHID const&&) = delete;
+    void operator=(MultiHID const&&) = delete;
     const uint8_t* report_desc() override; // returns pointer to the report descriptor; Warning: this method must store the length of the report descriptor in reportLength
     bool sendReport(JoystickData& joystickData);
 protected:
     const uint8_t* configuration_desc(uint8_t index) override;   // Get configuration descriptor; returns pointer to the configuration descriptor
     const uint8_t* string_iproduct_desc() override;      // Get string product descriptor
 private:
-    uint8_t configurationDescriptor[41];
+    static const uint8_t ConfigurationDescriptorSize = 41;
+    uint8_t configurationDescriptor[ConfigurationDescriptorSize];   // NOLINT
 };
 
 #endif /* USB_H_ */
