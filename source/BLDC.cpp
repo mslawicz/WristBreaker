5d8131c299a800e4d754b5f1b0f851b7eb273451
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-11-04 18:17:31 +0100

    fast Sine function added for motor calculations

--- source/BLDC.cpp
+++ source/BLDC.cpp
@@ -15,3 +15,43 @@ MotorBLDC::MotorBLDC(PinName outA, PinName outB, PinName outC, PinName enable) :
 {
     this->enable = 0;
 }
+
+// returns sine(argument)
+// argument: 0..90 degrees
+float MotorBLDC::fastSineD(float argument)
+{
+    static const float FullCycle = 360.0F;
+    static const float HalfCycle = 180.0F;
+    static const float QuarterCycle = 90.0F;
+    float sign = 1.0F;
+    
+    if (argument < 0)
+    {
+        argument = -argument;
+        sign = -sign;
+    }
+
+    if (argument >= FullCycle)
+    {
+        argument = std::fmod(argument, FullCycle);
+    }
+
+    if (argument >= HalfCycle)
+    {
+        argument -= HalfCycle;
+        sign = -sign;
+    }
+
+    if (argument >= QuarterCycle)
+    {
+        argument = HalfCycle - argument;
+    }
+
+    int lowerIndex = static_cast<int>(argument);
+    if (lowerIndex == (SineArraySize - 1))
+    {
+        return sign;
+    }
+
+    return sign * (SineLUT[lowerIndex] + (argument - static_cast<float>(lowerIndex)) * (SineLUT[lowerIndex + 1] - SineLUT[lowerIndex])); //NOLINT(cppcoreguidelines-pro-bounds-constant-array-index)
+}
