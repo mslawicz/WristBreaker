2dcc12e39540aaae3f8b73be0ae2f76d582d7547
Marcin Sławicz <marcin.slawicz@gmail.com>
2020-11-04 09:09:01 +0100

    BLDC files added

---
+++ source/BLDC.cpp
@@ -0,0 +1,8 @@
+/*
+ * BLDC.cpp
+ *
+ *  Created on: 04.11.2020
+ *      Author: Marcin
+ */
+
+#include "BLDC.h"
---
+++ source/BLDC.h
@@ -0,0 +1,18 @@
+/*
+ * BLDC.h
+ *
+ *  Created on: 04.11.2020
+ *      Author: Marcin
+ */
+
+#ifndef BLDC_H_
+#define BLDC_H_
+
+#include <mbed.h>
+
+class MotorBLDC
+{
+
+};
+
+#endif /* BLDC_H_ */
