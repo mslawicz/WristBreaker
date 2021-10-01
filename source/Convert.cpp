57427526d531ed1f58f669308a7f1498d0f1ed2b
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-10-01 22:56:33 +0200

    added some modifications from lazyPhase branch

--- source/Convert.cpp
+++ source/Convert.cpp
@@ -18,3 +18,21 @@ float cropAngle(float angle)
     }
     return fmodf(angle, FullAngle);
 }
+
+//calculate difference between 2 angles in the range 0-360
+float angleDifference(float angle1, float angle2)
+{
+    const float HalfCycle = 180.0F;    // 1/2 of electric cycle in degrees
+    const float FullCycle = 360.0F;    // full electric cycle in degrees
+    float dAngle = angle1 - angle2;
+    //the difference must be in the range <-180,180>
+    if(dAngle > HalfCycle)
+    {
+        dAngle -= FullCycle;
+    } 
+    else if(dAngle < -HalfCycle)
+    {
+        dAngle += FullCycle;
+    }
+    return dAngle;
+}
