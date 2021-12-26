a12578826b07ae8dd14b396f1ef68275e7034132
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-12-26 19:14:46 +0100

    Actuator files added

---
+++ source/Actuator.cpp
@@ -0,0 +1,8 @@
+/*
+ * Actuator.cpp
+ *
+ *  Created on: 26.12.2021
+ *      Author: Marcin
+ */
+
+#include "Actuator.h"
---
+++ source/Actuator.h
@@ -0,0 +1,16 @@
+/*
+ * ACTUATOR.h
+ *
+ *  Created on: 26.12.2021
+ *      Author: Marcin
+ */
+
+#ifndef ACTUATOR_H_
+#define ACTUATOR_H_
+
+class Actuator
+{
+
+};
+
+#endif /* ACTUATOR_H_ */
