6aa62709356c4c1c1e7cd85d603e192814b016a2
Marcin Sławicz <marcin.slawicz@gmail.com>
2021-12-27 14:15:02 +0100

    Stepper files and class added

---
+++ source/Stepper.cpp
@@ -0,0 +1,8 @@
+/*
+ * Stepper.cpp
+ *
+ *  Created on: 27.12.2021
+ *      Author: Marcin
+ */
+
+#include "Stepper.h"
---
+++ source/Stepper.h
@@ -0,0 +1,18 @@
+/*
+ * Stepper.h
+ *
+ *  Created on: 27.12.2021
+ *      Author: Marcin
+ */
+
+#ifndef STEPPER_H_
+#define STEPPER_H_
+
+#include "Motor.h"
+#include <mbed.h>
+
+class Stepper : public Motor
+{
+};
+
+#endif /* STEPPER_H_ */
